b4eceac7770a6f99d59d1e200c3e7cac7e795aae
Dario Dematties <djdematties@gmail.com>
2023-01-31 10:39:03 -0600

    Fixing error on plugin scope

--- visualize_attention.py
+++ visualize_attention.py
@@ -151,86 +151,87 @@ if __name__ == '__main__':
         else:
             print("There is no reference weights available for this model => We use random weights.")
 
-    # open image
-    if args.image_path is None:
-        ## user has not specified any image - we use our own image
-        #print("Please use the `--image_path` argument to indicate the path of the image you wish to visualize.")
-        #print("Since no image path have been provided, we take the first image in our paper.")
-        #response = requests.get("https://dl.fbaipublicfiles.com/dino/img.png")
-        #img = Image.open(BytesIO(response.content))
-        #img = img.convert('RGB')
-        if args.input:
-            with Plugin() as plugin, Camera(args.input) as camera:
-                # process samples from video stream
-                sample = camera.snapshot()
-                sample.save("live_image.jpg")
-                args.image_path = "live_image.jpg"
-                with open(args.image_path, 'rb') as f:
-                    img = Image.open(f)
-                    img = img.convert('RGB')
+    with Plugin() as plugin:
+        # open image
+        if args.image_path is None:
+            ## user has not specified any image - we use our own image
+            #print("Please use the `--image_path` argument to indicate the path of the image you wish to visualize.")
+            #print("Since no image path have been provided, we take the first image in our paper.")
+            #response = requests.get("https://dl.fbaipublicfiles.com/dino/img.png")
+            #img = Image.open(BytesIO(response.content))
+            #img = img.convert('RGB')
+            if args.input:
+                with Camera(args.input) as camera:
+                    # process samples from video stream
+                    sample = camera.snapshot()
+                    sample.save("live_image.jpg")
+                    args.image_path = "live_image.jpg"
+                    with open(args.image_path, 'rb') as f:
+                        img = Image.open(f)
+                        img = img.convert('RGB')
+            else:
+                with Camera() as camera:
+                    # process samples from video stream
+                    sample = camera.snapshot()
+                    sample.save("live_image.jpg")
+                    args.image_path = "live_image.jpg"
+                    with open(args.image_path, 'rb') as f:
+                        img = Image.open(f)
+                        img = img.convert('RGB')
+        elif os.path.isfile(args.image_path):
+            with open(args.image_path, 'rb') as f:
+                img = Image.open(f)
+                img = img.convert('RGB')
         else:
-            with Plugin() as plugin, Camera() as camera:
-                # process samples from video stream
-                sample = camera.snapshot()
-                sample.save("live_image.jpg")
-                args.image_path = "live_image.jpg"
-                with open(args.image_path, 'rb') as f:
-                    img = Image.open(f)
-                    img = img.convert('RGB')
-    elif os.path.isfile(args.image_path):
-        with open(args.image_path, 'rb') as f:
-            img = Image.open(f)
-            img = img.convert('RGB')
-    else:
-        print(f"Provided image path {args.image_path} is non valid.")
-        sys.exit(1)
-    transform = pth_transforms.Compose([
-        pth_transforms.Resize(args.image_size),
-        pth_transforms.ToTensor(),
-        pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-    img = transform(img)
+            print(f"Provided image path {args.image_path} is non valid.")
+            sys.exit(1)
+        transform = pth_transforms.Compose([
+            pth_transforms.Resize(args.image_size),
+            pth_transforms.ToTensor(),
+            pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+        img = transform(img)
 
-    # make the image divisible by the patch size
-    w, h = img.shape[1] - img.shape[1] % args.patch_size, img.shape[2] - img.shape[2] % args.patch_size
-    img = img[:, :w, :h].unsqueeze(0)
+        # make the image divisible by the patch size
+        w, h = img.shape[1] - img.shape[1] % args.patch_size, img.shape[2] - img.shape[2] % args.patch_size
+        img = img[:, :w, :h].unsqueeze(0)
 
-    w_featmap = img.shape[-2] // args.patch_size
-    h_featmap = img.shape[-1] // args.patch_size
+        w_featmap = img.shape[-2] // args.patch_size
+        h_featmap = img.shape[-1] // args.patch_size
 
-    attentions = model.get_last_selfattention(img.to(device))
+        attentions = model.get_last_selfattention(img.to(device))
 
-    nh = attentions.shape[1] # number of head
+        nh = attentions.shape[1] # number of head
 
-    # we keep only the output patch attention
-    attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
+        # we keep only the output patch attention
+        attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
 
-    if args.threshold is not None:
-        # we keep only a certain percentage of the mass
-        val, idx = torch.sort(attentions)
-        val /= torch.sum(val, dim=1, keepdim=True)
-        cumval = torch.cumsum(val, dim=1)
-        th_attn = cumval > (1 - args.threshold)
-        idx2 = torch.argsort(idx)
-        for head in range(nh):
-            th_attn[head] = th_attn[head][idx2[head]]
-        th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
-        # interpolate
-        th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
+        if args.threshold is not None:
+            # we keep only a certain percentage of the mass
+            val, idx = torch.sort(attentions)
+            val /= torch.sum(val, dim=1, keepdim=True)
+            cumval = torch.cumsum(val, dim=1)
+            th_attn = cumval > (1 - args.threshold)
+            idx2 = torch.argsort(idx)
+            for head in range(nh):
+                th_attn[head] = th_attn[head][idx2[head]]
+            th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
+            # interpolate
+            th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
 
-    attentions = attentions.reshape(nh, w_featmap, h_featmap)
-    attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
+        attentions = attentions.reshape(nh, w_featmap, h_featmap)
+        attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
 
-    # save attentions heatmaps
-    os.makedirs(args.output_dir, exist_ok=True)
-    torchvision.utils.save_image(torchvision.utils.make_grid(img, normalize=True, scale_each=True), os.path.join(args.output_dir, "img.png"))
-    for j in range(nh):
-        fname = os.path.join(args.output_dir, "attn-head" + str(j) + ".png")
-        plt.imsave(fname=fname, arr=attentions[j], format='png')
-        print(f"{fname} saved.")
-        plugin.upload_file(fname, timestamp=sample.timestamp)
-
-    if args.threshold is not None:
-        image = skimage.io.imread(os.path.join(args.output_dir, "img.png"))
+        # save attentions heatmaps
+        os.makedirs(args.output_dir, exist_ok=True)
+        torchvision.utils.save_image(torchvision.utils.make_grid(img, normalize=True, scale_each=True), os.path.join(args.output_dir, "img.png"))
         for j in range(nh):
-            display_instances(image, th_attn[j], fname=os.path.join(args.output_dir, "mask_th" + str(args.threshold) + "_head" + str(j) +".png"), blur=False)
+            fname = os.path.join(args.output_dir, "attn-head" + str(j) + ".png")
+            plt.imsave(fname=fname, arr=attentions[j], format='png')
+            print(f"{fname} saved.")
+            plugin.upload_file(fname, timestamp=sample.timestamp)
+
+        if args.threshold is not None:
+            image = skimage.io.imread(os.path.join(args.output_dir, "img.png"))
+            for j in range(nh):
+                display_instances(image, th_attn[j], fname=os.path.join(args.output_dir, "mask_th" + str(args.threshold) + "_head" + str(j) +".png"), blur=False)
